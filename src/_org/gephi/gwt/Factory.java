dff9603b41532bee9fcbfd907b8c11fb4e6f4ddd
jldupont <github@jldupont.com>
2011-08-01 19:54:34 -0400

    _org.gephi

--- src/_org/gephi/gwt/Factory.java
+++ src/_org/gephi/gwt/Factory.java
@@ -1,20 +1,55 @@
+/**
+ * @author jldupont
+ */
 package _org.gephi.gwt;
 
 import java.util.HashMap;
 
+import org.gephi.project.api.ProjectController;
+
+import com.google.gwt.core.client.GWT;
+
 public class Factory {
 
+	static enum Classe {
+		PROJECT_CONTROLLER,
+	}
+	
 	@SuppressWarnings("unchecked")
-	HashMap<Class, Object> map=new HashMap<Class, Object>();
+	static HashMap<Class, Object> map=new HashMap<Class, Object>();
+	
+	@SuppressWarnings("unchecked")
+	static HashMap<Class, Classe> cmap=new HashMap<Class, Classe>();
 	
 	void init() {
 		
-		//map.put(arg0, arg1);
+		map.put(ProjectController.class, null);
+		cmap.put(ProjectController.class, Classe.PROJECT_CONTROLLER);
+		
 	}
 	
-	public static <T> T get(Class<T> klass) {
+	@SuppressWarnings("unchecked")
+	public static <T> T get(Class<T> klass){
 		
-		return null;
+		if (map.containsKey(klass)) {
+			Object k=map.get(klass);
+			if (k!=null) {
+				return (T) k;
+			}
+		} else {
+			throw new RuntimeException("Unknown class: "+klass.toString());
+		}
+		// instance doesn't exist yet... create it!
+		Classe c=cmap.get(klass);
+		
+		Object o=null;
+		
+		switch(c) {
+		case PROJECT_CONTROLLER:	o=GWT.create(ProjectController.class); break;
+		
+		}
+		
+		return (T) o;
 	}//
 	
 }//
---
+++ test/test/org/gephi/project/Project.java
@@ -0,0 +1,22 @@
+package test.org.gephi.project;
+
+import org.gephi.project.api.ProjectController;
+import org.openide.util.Lookup;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class Project extends GWTTestCase {
+
+	public String getModuleName() {
+		return "test.org.gephi.project.Project";
+	}
+
+	public void testCreate() {
+		
+		ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
+		
+		assertTrue(true);
+	}
+	
+	
+}///
